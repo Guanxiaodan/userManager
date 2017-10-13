9680ab79fe06d290b154b795289fef276367645c
关晓丹 <guanxiaodan@magicmind.cn>
2017-10-13 19:30:03 +0800

    s

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -18,6 +17,89 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+}
+/*垂直布局*/
+.flex-c {
+  display: flex;
+  flex-direction: column;
+}
+
+/*水平布局*/
+.flex-r {
+  display: flex;
+  flex-direction: row;
+}
+
+/*主轴方向元素居中*/
+.m-center {
+  justify-content: center;
+}
+
+/*交叉方向元素居中*/
+.x-center {
+  align-items: center;
+}
+
+/*上下左右居中*/
+.center {
+  justify-content: center;
+  align-items: center;
+}
+
+/*主轴方向平均分配*/
+.m-around {
+  justify-content: space-around;
+}
+
+/*主轴方向顶边分配*/
+.between {
+  justify-content: space-between;
+}
+
+.to-top {
+  margin-top: 1rem;
+}
+
+.to-top-large {
+  margin-top: 2rem;
+}
+
+.to-left {
+  margin-left: 1rem;
+}
+
+.to-left-large {
+  margin-left: 2rem;
+}
+
+.to-right {
+  margin-right: 1rem;
+}
+
+.to-right-large {
+  margin-right: 2rem;
+}
+
+.to-bottom {
+  margin-bottom: 1rem;
+}
+
+.to-bottom-large {
+  margin-bottom: 2rem;
+}
+
+.cover {
+  width: 100%;
+  height: 100%;
+}
+
+.pointer {
+  cursor: pointer;
+}
+
+.shadow {
+  box-shadow: 0 0 2px rgba(0, 0, 0, .3), 0 1px 8px rgba(0, 0, 0, .3);
 }
 </style>
---
+++ src/pages/signup.vue
@@ -0,0 +1,72 @@
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container-all {
+    width: 100%;
+    height: 100%;
+    background: url("../../static/city.jpg") no-repeat;
+    /*background: url("https://whvn.cc/338001") no-repeat;*/
+    background-size: cover;
+  }
+
+  .container-border {
+    box-shadow: 0 0 2px rgba(255, 255, 255, .7), 0 1px 8px rgba(0, 0, 0, .3);
+    border: 1px solid #ccc;
+    width: 20rem;
+    height: 20rem;
+    border-radius: 1rem;
+  }
+
+  .container-border:hover {
+    box-shadow: 0 0 2px rgba(0, 0, 0, .3), 0 1px 8px rgba(0, 0, 0, .3);
+
+  }
+  .rice-yellow {
+    color: #fff8ba;
+  }
+  .green {
+    color: #0deac9;
+  }
+</style>
+
+<template>
+    <div class="flex-c center container-all">
+      <div>
+        <div class="container-border flex-c m-around ">
+          <div>
+            <h2 class="rice-yellow">请输入用户名：</h2>
+            <Input class="to-top" v-model="name" placeholder="请输入用户名..." style="width: 300px"></Input>
+          </div>
+          <div>
+            <h2 class="rice-yellow">请输入密码：</h2>
+            <Input class="to-top" v-model="password" placeholder="请输入密码..." type="password" style="width: 300px"></Input>
+          </div>
+          <div class="green pointer">
+            <span>还没有账号?</span>
+            <span>点击注册>></span>
+          </div>
+          <div>
+            <Button type="primary" @click="login">登 录</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+  const debug = require('debug')('login')
+//  const _ = require('lodash');
+
+  export default {
+    data () {
+      return {
+        name: '',
+        password: ''
+      }
+    },
+    methods: {
+      login () {
+        debug('点击了登录按妞')
+      }
+    }
+  }
+</script>

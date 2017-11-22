bc4dbc547ad6bef3e8cbc5adec897e8204e94d19
关晓丹 <GXD9983@163.com>
2017-11-22 13:15:48 +0800

    session

--- service/index.js
+++ service/index.js
@@ -4,9 +4,11 @@
  */
 var express = require('express')
 var app = express()
+var mongoose = require('mongoose')
 const debug = require('debug')('index')
 const bodyParser = require('body-parser')
-// const session = require('express-session')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
 /**
  *   解决跨域问题
@@ -31,11 +33,42 @@ app.use('/', (req, res, next) => {
 app.listen(9983, () => {})
 app.use(bodyParser.json())
 
+
+
 /**
  * 使用mongoose数据库
  */
-var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/userManager') // 将userManager数据库挂载到localhost上运行,如果没有该数据库就创建
+
+/**
+ * 使用session
+ */
+app.use(session({
+  secret: 'keyboard cat',
+  store: new MongoStore({
+    host: 'localhost',
+    port: 27017,
+    // dbPromise: 'userManager'
+    db: mongoose.connection.db
+  }),
+  cookie: {
+    maxAge: 1000 * 60 * 5
+  }
+}))
+
+app.get('/', (req, res, next) => { // 接入session数据
+  debug('进来了吗，这里是session')
+  var sess = req.session
+  if (sess.views) {
+    sess.views++
+    res.setHeader('Content-Type', 'text/html')
+    res.write(`这是您第${sess.views}次登陆，距离过期时间还有${sess.cookie.maxAge / 1000}秒`)
+    res.end()
+  } else {
+    sess.views = 1
+    res.end('欢迎来到session存储')
+  }
+})
 
 // 现在需要检验数据库是否连接成功
 var db = mongoose.connection
@@ -72,7 +105,7 @@ db.once('open', () => {
   /**
    * 登录
    */
-  app.post('/signIn', (req, res) => {
+  app.post('/signIn2', (req, res) => {
     debug('这个是登录时浏览器发送过来的数据', req.body)
     User.findOne({name: req.body.name}, (err, persion) => {
       if (err) {
@@ -87,9 +120,16 @@ db.once('open', () => {
         if (persion.pwd !== req.body.pwd) {
           res.status(200).send('密码错误')
         } else {
-          res.status(200).send('登陆成功')
+          debug('什么情况？？？')
+          req.session.userInfo = {}
+          req.session.userInfo.name = req.body.name
+          res.status(200).send('登录成功')
         }
       }
     })
+  })
+  app.post('/signIn', (req, res) => {
+    debug('这个是登录时浏览器发送过来的数据', req.body)
+    res.status(200).send('登录成功')
   })
 })
